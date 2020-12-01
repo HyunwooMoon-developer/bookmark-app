428325625c685dd70d8189cfd533cb011dd1a8fa
Hyunwoo Moon <mhw9163@gmail.com>
2020-11-30 17:36:43 -0800

    working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
+import $ from 'jquery';
 
 const Bookmark = [{id: cuid(), title: 'google', url: 'http://google.com', desc:'google', rate :3, expanded: false}];
 
